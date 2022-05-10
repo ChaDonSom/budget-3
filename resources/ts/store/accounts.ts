24267cac1b4cb6320b63cee8f7e6cc7e92b4dbfb
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-05-10 10:46:51 -0400

    Fix accounts store

--- resources/ts/store/accounts.ts
+++ resources/ts/store/accounts.ts
@@ -1,24 +1,26 @@
 import { toReactive } from "@vueuse/core";
 import axios from "axios"
-import { ComponentPropsOptions, computed, reactive, ref, SetupContext } from "vue"
+import { ComponentPropsOptions, computed, ComputedRef, reactive, ref, SetupContext, toRefs } from "vue"
 
 export function useAccounts(props?: ComponentPropsOptions, context?: SetupContext) {
-    type AccountsData = {
-        [key: string|number]: { id: number; name: string; amount: number }
-    }
+    type Account = { id: number; name: string; amount: number }
+    type AccountsData = { [key: string|number]: Account }
     const data = ref<AccountsData>({})
 
     const keys   = computed(() => Object.keys(data.value))
-    const values = computed(() => Object.values(data.value))
+    const values: ComputedRef<Account[]> = computed(() => Object.values(data.value))
 
     async function getData() {
         let response = await axios.get('/api/accounts')
         data.value = response.data as AccountsData
     }
-    return toReactive({
-        data,
-        keys,
-        values,
+    
+    return {
+        ...toRefs({
+            data,
+            keys,
+            values,
+        }),
         getData,
-    })
+    }
 }
