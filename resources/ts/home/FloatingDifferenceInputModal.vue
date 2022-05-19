a1daef436e23592d8415e16dcfcde6113df27c66
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-05-18 20:37:54 -0400

    resolve #6

--- resources/ts/home/FloatingDifferenceInputModal.vue
+++ resources/ts/home/FloatingDifferenceInputModal.vue
@@ -13,9 +13,15 @@
         @keydown-enter="modals.close(id)"
         class="difference-field"
     >Amount</DollarsField>
-    <div class="flex justify-between px-4" v-if="!modifying">
+    <div class="flex justify-between px-4 mb-5" v-if="!modifying">
       <IconButton :density="-3" @click.stop="modifying = difference.modifier == -1 ? 1 : -1">remove</IconButton>
       <IconButton :density="-3" @click.stop="modifying = difference.modifier">add</IconButton>
+    </div>
+    <div class="flex justify-between px-4" v-if="!modifying">
+      <IconButton :density="-3" @click.stop="modifying = '/'">
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M19 13H5v-2h14v2m-7-8a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0 10a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2Z"/></svg>
+      </IconButton>
+      <IconButton :density="-3" @click.stop="modifying = 'x'">close</IconButton>
     </div>
     <DollarsField
         v-if="modifying"
@@ -54,13 +60,19 @@ const emit = defineEmits(['close'])
 
 const modals = useModals()
 
-const modifying = ref<1|-1|null>(null)
+const modifying = ref<1|-1|'x'|'/'|null>(null)
 const modifyAmount = ref(0)
 function modify() {
   if (modifying.value) {
     let trueAmount = props.difference.amount * props.difference.modifier
-    trueAmount += (modifyAmount.value * modifying.value)
-    props.difference.amount = Math.abs(trueAmount)
+    if (modifying.value == 1 || modifying.value == -1) {
+      trueAmount += (modifyAmount.value * modifying.value)
+    } else if (modifying.value == 'x') {
+      trueAmount = trueAmount * modifyAmount.value
+    } else if (modifying.value == '/') {
+      trueAmount = Math.ceil((trueAmount / modifyAmount.value) * 100) / 100
+    }
+    props.difference.amount = Math.abs(Math.round(trueAmount * 100) / 100)
     props.difference.modifier = trueAmount >= 0 ? 1 : -1
   }
   modifying.value = null
