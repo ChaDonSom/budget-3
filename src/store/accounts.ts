892aa084085e66326d8b2b0603c54377e413bab4
Chase Macbook Air <chadonsom@gmail.com>
2024-07-20 22:48:39 -0400

    ts

--- src/store/accounts.ts
+++ src/store/accounts.ts
@@ -1,71 +1,74 @@
-import { useAuth, type User } from "@/core/users/auth";
-import type { BatchUpdate, BatchUpdateWithAccounts } from "@/store/batchUpdates";
-import { useBatchUpdates } from "@/store/batchUpdates";
-import { useEcho } from "@/store/echo";
-import axios, { type AxiosResponse } from "axios";
-import { defineStore } from "pinia";
-import {
-    computed,
-    reactive,
-    ref,
-    toRefs,
-    watch,
-} from "vue";
-import type {
-    ComponentPropsOptions,
-    ComputedRef,
-    SetupContext,
-} from "vue"
+import { useAuth, type User } from "@/core/users/auth"
+import type { BatchUpdate, BatchUpdateWithAccounts } from "@/store/batchUpdates"
+import { useBatchUpdates } from "@/store/batchUpdates"
+import { useEcho } from "@/store/echo"
+import axios, { type AxiosResponse } from "axios"
+import { defineStore } from "pinia"
+import { computed, reactive, ref, toRefs, watch } from "vue"
+import type { ComponentPropsOptions, ComputedRef, SetupContext } from "vue"
 
 export type Account = {
-    id: number;
-    name: string;
-    amount: number;
-    user_id: number;
-    note: string;
-    batch_updates?: (BatchUpdate & { pivot: { amount: number }})[];
-    favorited_users?: User[];
-};
+    id: number
+    name: string
+    amount: number
+    user_id: number
+    note: string
+    batch_updates?: (BatchUpdate & { pivot: { amount: number } })[]
+    favorited_users?: User[]
+    isBatchUpdate?: boolean
+}
 export type AccountWithBatchUpdates = Account & {
-    batch_updates: (BatchUpdate & { pivot: { amount: number } })[];
-};
-export type AccountsData = { [key: string | number]: Account };
+    batch_updates: (BatchUpdate & { pivot: { amount: number } })[]
+}
+export type AccountsData = { [key: string | number]: Account }
 
-export const data = ref<AccountsData>({});
+export const data = ref<AccountsData>({})
 
 export const keys = computed(() => {
     let v = data.value
     return Object.keys(v)
-});
+})
 export const values: ComputedRef<Account[]> = computed(() => {
     let v = data.value
     return Object.values(v)
-});
+})
 
 export async function fetchData(query?: { [key: string]: any }) {
-    let urlQuery = query ? `?${Object.keys(query).map(k => `${k}=${query[k]}`).join('&')}` : ''
-    let response: AxiosResponse<AccountsData> = await axios.get(`/api/accounts${urlQuery}`);
+    let urlQuery = query
+        ? `?${Object.keys(query)
+              .map((k) => `${k}=${query[k]}`)
+              .join("&")}`
+        : ""
+    let response: AxiosResponse<AccountsData> = await axios.get(
+        `/api/accounts${urlQuery}`
+    )
     data.value = response.data
     const echo = useEcho()
     const auth = useAuth()
     const batchUpdates = useBatchUpdates()
-    echo.echo.private(`AccountBatchUpdates.User.${auth.user?.id}`).listen(
-        'AccountBatchUpdateSaved',
-        (payload: { update: BatchUpdateWithAccounts }) => {
-            console.log(`Got broadcast for batchUpdate ${payload.update.id}, updating in store...`)
-            batchUpdates.receive(payload.update)
-            for (let account of payload.update.accounts) {
-                data.value[account.id].amount = account.amount
+    echo.echo
+        .private(`AccountBatchUpdates.User.${auth.user?.id}`)
+        .listen(
+            "AccountBatchUpdateSaved",
+            (payload: { update: BatchUpdateWithAccounts }) => {
+                console.log(
+                    `Got broadcast for batchUpdate ${payload.update.id}, updating in store...`
+                )
+                batchUpdates.receive(payload.update)
+                for (let account of payload.update.accounts) {
+                    data.value[account.id].amount = account.amount
+                }
             }
-        }
-    )
+        )
 }
 
 export async function fetchAccount(id: number) {
-    let response: AxiosResponse<Account> = await axios.get(`/api/accounts/${id}`)
+    let response: AxiosResponse<Account> = await axios.get(
+        `/api/accounts/${id}`
+    )
     data.value = {
         ...data.value,
-        [id]: response.data
+        [id]: response.data,
     }
     return data.value[id]
 }
@@ -77,7 +80,7 @@ export async function fetchAccounts(ids: number[]) {
 export async function receive(account: Account) {
     data.value = {
         ...data.value,
-        [account.id]: account
+        [account.id]: account,
     }
     return data.value[account.id]
 }
@@ -86,7 +89,10 @@ export async function remove(id: number) {
     delete data.value[id]
 }
 
-export function useAccounts(props?: ComponentPropsOptions, context?: SetupContext) {
+export function useAccounts(
+    props?: ComponentPropsOptions,
+    context?: SetupContext
+) {
     return reactive({
         data,
         keys,
@@ -99,7 +105,7 @@ export function useAccounts(props?: ComponentPropsOptions, context?: SetupContex
     })
 }
 
-export const useAccountsStore = defineStore('accounts', () => {
+export const useAccountsStore = defineStore("accounts", () => {
     return {
         data,
         keys,
@@ -109,5 +115,5 @@ export const useAccountsStore = defineStore('accounts', () => {
         fetchAccounts,
         receive,
         remove,
-    };
+    }
 })
