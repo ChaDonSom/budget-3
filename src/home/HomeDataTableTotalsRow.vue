2063f1ad70eaf65cbfa57d62898831d242fa56f8
Chase Macbook Air <chadonsom@gmail.com>
2025-07-26 22:33:04 -0400

    fix totals row display label and value

--- src/home/HomeDataTableTotalsRow.vue
+++ src/home/HomeDataTableTotalsRow.vue
@@ -1,5 +1,26 @@
 <script lang="ts" setup>
 import DataTableRow from "@/core/tables/DataTableRow.vue"
+import { columnsToShow } from "."
+import { dollars } from "@/core/utilities/currency"
+import { computed } from "vue"
+import DataTableCell from "@/core/tables/DataTableCell.vue"
+
+defineProps<{
+    /**
+     * The account object containing the totals. Shows 'nextAmount' meaning the total of the accounts' next amounts
+     * it's for.
+     */
+    account: {
+        nextAmount: number
+    }
+}>()
+
+const columnKeys = computed(
+    () =>
+        Object.keys(
+            columnsToShow.value
+        ) as (keyof (typeof columnsToShow)["value"])[]
+)
 </script>
 
 <template>
@@ -7,6 +28,16 @@ import DataTableRow from "@/core/tables/DataTableRow.vue"
         style="height: unset"
         class="text-xl italic border-t-2 text-slate-600 border-slate-400"
     >
-        <slot />
+        <DataTableCell
+            v-for="column of columnKeys"
+            :key="column"
+            :numeric="column === 'nextAmount'"
+            :class="{ hidden: !columnsToShow[column] }"
+        >
+            <span v-if="column === 'name'">Totals</span>
+            <div v-else-if="column === 'nextAmount'" class="whitespace-nowrap">
+                {{ dollars(account.nextAmount / 100) }}
+            </div>
+        </DataTableCell>
     </DataTableRow>
 </template>
